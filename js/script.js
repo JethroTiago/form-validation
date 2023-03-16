446ed7e4c3f78b405a0bbb27ad6e583502e2b1fa
Jethro <jethrotiago@hotmail.com>
2023-03-16 15:28:19 -0300

    Added set custom validity message

--- js/script.js
+++ js/script.js
@@ -48,6 +48,7 @@ const mensagens = {
 
 function verificaCampo(campo) {
     let mensagem = "";
+    campo.setCustomValidity('');
     if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPF(campo);
     }
