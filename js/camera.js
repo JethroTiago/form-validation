110c3cc8356b1853ea0b463ba11ccce3e616316e
Jethro <jethrotiago@hotmail.com>
2023-03-16 16:59:25 -0300

    Created variables with the data attributes

---
+++ js/camera.js
@@ -0,0 +1,3 @@
+const botaoIniciarCamera = document.querySelector("[data-video-botao]");
+const campoCmaera = document.querySelector("[data-camera]");
+const video = docuemnt.querySelector("[data-video]");
