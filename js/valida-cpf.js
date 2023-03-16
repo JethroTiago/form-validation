19d6f7537fe90e7ca838c4014a6de090f6c6ad84
Jethro <jethrotiago@hotmail.com>
2023-03-16 15:27:54 -0300

    Added set custom validity message

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,9 +1,7 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
     if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        console.log("Este CPF não existe");
-    } else {
-        console.log("Este CPF existe");
+        campo.setCustomValidity('Este CPf não é válido!')
     }
 }
 
