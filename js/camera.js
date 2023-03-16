1124d66848e109656f4435fe8156bf652a6005f9
Jethro <jethrotiago@hotmail.com>
2023-03-16 17:19:29 -0300

    Activating camera on click

--- js/camera.js
+++ js/camera.js
@@ -1,3 +1,13 @@
 const botaoIniciarCamera = document.querySelector("[data-video-botao]");
-const campoCmaera = document.querySelector("[data-camera]");
+const campoCamera = document.querySelector("[data-camera]");
 const video = docuemnt.querySelector("[data-video]");
+
+botaoIniciarCamera.addEventListener("click", async function () {
+    const iniciarVideo = await navigator.mediaDevices
+    .getUserMedia({video: true, audio: false})
+
+    botaoIniciarCamera.style.display = "none";
+    campoCamera.style.display = "block";
+
+    video.srcObject = iniciarVideo;
+})
